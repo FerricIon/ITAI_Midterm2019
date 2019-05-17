75728141f1541f6b2cc5996f8508fcf4eee40677
Jeff0115 <50329502+Jeff0115@users.noreply.github.com>
2019-05-17 12:36:20 +0800

    协同过滤（无用户版本）

--- server/python/recommend function.py
+++ server/python/recommend function.py
@@ -143,6 +143,55 @@ def language_compare(film,data_list):
     same_set=set(film.spoken_languages).intersection(set(data_list))
     return len(same_set)
 
+#cf协同过滤:建立电影关联度矩阵cf_BasedOnMovie_sim.json，由属性相关度和用户同时喜爱数决定，后者暂无数据
+'''def cfkeyword_compare(f1,f2):
+    same_set=f1.root.intersection(f2.root)
+    return len(same_set)
+cf_temp=0
+cf_BasedOnMovie_sim={}
+cmsf=open('../data/cf_BasedOnMovie_sim.json','w')
+for f1 in all_movies:
+    for f2 in all_movies:
+        if f1.ID < f2.ID:
+            if f1.ID not in cf_BasedOnMovie_sim.keys():
+                cf_BasedOnMovie_sim[f1.ID]=[]
+            if f2.ID not in cf_BasedOnMovie_sim.keys():
+                cf_BasedOnMovie_sim[f2.ID]=[]
+            cf_temp=cfkeyword_compare(f1,f2)+genre_compare(f1,f2.genre)+cast_compare(f1,f2.cast)+crew_compare(f1,f2.crew)+company_compare(f1,f2.company)+language_compare(f1,f2.spoken_languages)
+            if len(cf_BasedOnMovie_sim[f1.ID])<40:
+                cf_BasedOnMovie_sim[f1.ID].append([f2.ID,cf_temp])
+                if len(cf_BasedOnMovie_sim[f1.ID])==40:
+                    cf_BasedOnMovie_sim[f1.ID].sort(key=lambda it: it[1],reverse=True)
+            if len(cf_BasedOnMovie_sim[f2.ID])<40:
+                cf_BasedOnMovie_sim[f2.ID].append([f1.ID,cf_temp])
+                if len(cf_BasedOnMovie_sim[f2.ID])==40:
+                    cf_BasedOnMovie_sim[f2.ID].sort(key=lambda it: it[1],reverse=True)
+            if len(cf_BasedOnMovie_sim[f1.ID])>=40 and cf_temp>cf_BasedOnMovie_sim[f1.ID][39][1]:
+                cf_BasedOnMovie_sim[f1.ID][39] = [f2.ID,cf_temp]
+                cf_BasedOnMovie_sim[f1.ID].sort(key=lambda it: it[1],reverse=True)
+            if len(cf_BasedOnMovie_sim[f2.ID])>=40 and cf_temp>cf_BasedOnMovie_sim[f2.ID][39][1]:
+                cf_BasedOnMovie_sim[f2.ID][39] = [f1.ID,cf_temp]
+                cf_BasedOnMovie_sim[f2.ID].sort(key=lambda it: it[1],reverse=True)
+cffile_w=js.dumps(cf_BasedOnMovie_sim)
+cmsf.write(cffile_w)
+cmsf.close()
+'''
+cmsf=open('../data/cf_BasedOnMovie_sim.json','r')
+cf_base=js.loads(cmsf)
+cmsf.close()
+cf_recommendList={}
+def cf_recommend(input_movieID_list):
+    for input_movieID in input_movieID_list:
+        for options_movie in cf_base[input_movieID]:
+            if options_movie[0] in cf_recommendList.keys():
+                cf_recommendList[options_movie[0]]+=options_movie[1]
+            else:
+                cf_recommendList[options_movie[0]]=options_movie[1]
+    cf_result=sorted(cf_recommendList.items(),key=lambda it:it[1],reverse=True)
+    cf_outputID=[]
+    cf_outputID.append(x[0] for x in cf_result[0:9])
+    return cf_outputID
+
 #推荐函数在这里写
 def recommendation(users_data):
     u_genre=users_data['genre'].copy()
